7a925b45b66679ddebbfcb3e83b82df2b30b9262
priyamadki <153053070+priyamadki@users.noreply.github.com>
2025-04-21 03:28:58 +0530

    Add files via upload

---
+++ R Basics.R
@@ -0,0 +1,178 @@
+"Hello World!"
+'Hello World'
+5
+10
+name1 = "Priya"
+name1
+name2 = "Gouri"
+name2
+age = 25
+age
+age = age + 5
+age
+x = 21
+x
+num = 20
+num
+num = num + 5
+4*8
+4+(2*5)
+(4+2)/3
+text = "Excellent"
+text
+paste("ExcelR is",text)
+paste("Students are",text)
+paste("My name is",name1)
+paste("Students are",text,"in class")
+var1 = var2 = var3 = "blue"
+var1
+var2
+var3
+myvar = "Priya"
+myvar
+MYVAR = "Neha"
+MYVAR
+myVar = "Preeti"
+myVar
+Myvar = "Sony"
+Myvar
+myvar2 = "Geeta"
+myvar2
+m2yvar = "sun"
+m2yvar
+myVar2 = "Alia"
+myVar2
+myvar_2 = "Om"
+myvar_2
+this.year = 2021
+this.year
+my_var = "sri"
+x =10.5
+class(x)
+class(name1)
+z = 10
+class(z)
+y = 5000L
+class(y)
+x = "R is exciting"
+class(x)
+5*3
+3>5
+5>3
+5==5
+1!=6
+x = 20
+y = 3
+x+y
+x-y
+x*y
+x/y
+x%%y
+x%/%y
+x^2
+x^y
+2*pi
+pi
+??constants
+LETTERS
+letters
+month.abb
+month.name
+pi
+max(25,78,10)
+min(2,10,3)
+sqrt(81)
+abs(-4.5)
+fruits = c("Banana","Apple","Mango","Orange")
+class(fruits)
+n1 = c(1,2,3,4)
+class(n1)
+n2 = c(T,F,TRUE,FALSE)
+class(n2)
+mix=c(81,20.5,TRUE,"Mango",5L,F)
+class(mix)
+x = c(2,5,8,4)
+y = c(1,9,9,9)
+x+y
+x-y
+x*y
+x/y
+x%%y
+x^y
+x*2
+(x+y)*10
+(x+y)*2
+1:10
+1:1000
+40:45
+seq(1,50,5)
+seq(1,50,by=3)
+seq(1,10)
+seq(10,1,-2)
+seq(10,1)
+n1 = 1.5:6.5
+n1
+rep(45,7)
+rep("Mango",8)
+sample(1:50,5)
+sample(1:10,20,replace=TRUE)
+sample(c("HP","Mango","Lenovo"),7,replace=TRUE)
+sample(c("HP","Mango","Lenovo"),2)
+x = c(2,5,8,20,10,30,58)
+x
+x[6]
+x[2]
+x[2:6]
+x[3:7]
+x[c(2,6)]
+x
+x[-1]
+x[-2]
+x
+x[-6]
+x[c(-1,-4)]
+x[c(-2,-6)]
+x[2]=15
+x
+x[-1]=5
+x[3]=10
+x
+x[4]=7
+x
+x[-3]
+x[1:3]
+3<5
+y=c(1,9,9,9)
+y
+y<9
+y[y<9]=10
+y
+y>7
+y[y>7]=12
+y
+marks = c(60,70,80,50,90)
+marks
+marks>50
+marks[marks>50]
+marks==80
+names=c("Om","Sri","Priya","Preeti")
+"Om" %in% names
+"Alia" %in% names
+marks
+marks[3:7]
+marks[4]
+marks[3]=97
+marks
+marks[-3]=100
+marks
+marks[8]=92
+marks
+price = c(2999,449,29,650,2800,192,9384,373,474,4745)
+price[3:7]
+price>1000
+price[price>1000]
+sort(price)
+price
+sort(price,decreasing=TRUE)
+length(price)
+
